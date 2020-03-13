ef852cfba1a7d188be015b3cc334b7d10d15cd5b
ckbhatia <chetansain86@gmail.com>
2020-03-13 23:42:16 +0530

    fix: replace the name and add some validations

--- utils/userValidation.js
+++ utils/userValidation.js
@@ -1,9 +1,13 @@
-module.exports = loginValidation = (email, username, password) => {
+module.exports = userValidation = (name, email, username, password) => {
   switch (true) {
+    case !name && name !== null:
+      return (msg = "Please enter the name");
     case !email && !username:
       return (msg = "Please enter either email or username.");
     case !password:
       return (msg = "Please enter password.");
+    case name && name.length < 4:
+      return (msg = "Name length error");
     case email && email.length < 12:
       return (msg = "Email length error");
     case username && username.length < 2:
