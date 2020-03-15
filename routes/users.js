36fd365952c0d4d61ecde19dfce9eee09b3bb179
ckbhatia <chetansain86@gmail.com>
2020-03-15 22:48:10 +0530

    feat: create users router

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
+const crudControllers = require("../controllers/crudControllers");
+const user = require("../models/user");
+const Auth = require("../utils/auth");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+/* Listen to post request on users/login route */
+router.post("/login", crudControllers(user, "login").getOne);
+
+/* Listen to post request on users/register route */
+router.post("/register", crudControllers(user, "createUser").createOne);
+
+router.use(Auth.verifyToken);
+
+/* Listen to get, patch and delete request on /users route */
+router
+  .route("/")
+  .get(crudControllers(user, "getCurrentUser").getOne)
+  .patch(crudControllers(user, "updateUser").updateOne)
+  .delete(crudControllers(user).removeOne);
 
 module.exports = router;
