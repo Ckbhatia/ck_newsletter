2de95e74803d701e1408cfc5e1cfd6fb34d1f219
ckbhatia <chetansain86@gmail.com>
2020-03-17 12:18:50 +0530

    fix: make minor changes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+const router = require("express").Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET static pages. */
+router.get("*", function(req, res) {
+  res.render("index", { title: "The newsletter app" });
 });
 
 module.exports = router;
