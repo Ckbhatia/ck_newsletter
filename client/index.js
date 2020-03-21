098ce163b9e518658a159ca3942579d6a4bbd67a
ckbhatia <chetansain86@gmail.com>
2020-03-21 13:40:00 +0530

    feat: add and wrap app with browser router

--- client/index.js
+++ client/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./components/App";
 import "./sass/index.scss";
 
 ReactDOM.render(
-  <div>
+  <BrowserRouter>
     <App />
-  </div>,
+  </BrowserRouter>,
   document.getElementById("root")
 );
