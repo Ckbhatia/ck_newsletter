a4c8d64e4303bb741d88e1f56751487ab40ced9f
ckbhatia <chetansain86@gmail.com>
2020-03-27 18:57:01 +0530

    feat: create a project component

---
+++ client/components/Project.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Container } from "@material-ui/core";
+import styled from "styled-components";
+import Table from "./Table";
+
+const Project = ({ match, getSelectedProject, projectData }) => {
+  useEffect(() => {
+    const id = match.params.id;
+    getSelectedProject(id);
+  }, []);
+
+  return (
+    <Div className="project-main-container">
+      <Container maxWidth="md">
+        <div className="project-header-container">
+          <h3 className="project-heading">Project</h3>
+        </div>
+        <div className="project-container">
+          <Table projectData={projectData} />
+        </div>
+        <div className="project-btn-container">
+          <Link
+            to={`/project/${projectData && projectData._id}/edit`}
+            className="project-btn btn-edit"
+          >
+            Edit
+          </Link>
+          <button className="project-btn btn-delete">Delete</button>
+        </div>
+      </Container>
+    </Div>
+  );
+};
+
+export default withRouter(Project);
+
+const Div = styled.div`
+  max-height: 120vh;
+  min-height: 85vh;
+  background-color: #f7fbfb;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .project-container {
+    -webkit-box-shadow: -1px 0px 5px 2px rgb(229, 229, 229);
+    -moz-box-shadow: -1px 0px 5px 2px rgb(229, 229, 229);
+    box-shadow: -1px 0px 5px 2px rgb(229, 229, 229);
+  }
+  .project-heading {
+    font-size: 2rem;
+    color: #40b9ff;
+    font-weight: 600;
+  }
+  .project-header-container {
+    margin-bottom: 2rem;
+  }
+  .project-btn-container {
+    margin-top: 2rem;
+    .project-btn {
+      font-size: 1.2rem;
+      color: #fff;
+      border-radius: 2px;
+      padding: 0.5rem 2rem;
+      padding: 0.5rem 1.2rem;
+      border: none;
+      color: white;
+      margin-right: 1rem;
+      cursor: pointer;
+    }
+    .btn-edit {
+      text-decoration: none;
+      background-color: #40b9ff;
+      &:hover {
+        background-color: #3daef0;
+      }
+    }
+    .btn-delete {
+      background-color: #ff3636;
+      &:hover {
+        background-color: #ea3232;
+      }
+    }
+  }
+`;
