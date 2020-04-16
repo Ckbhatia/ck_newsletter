099980be9c979ac4ece88bef547cf801971bab19
ckbhatia <chetansain86@gmail.com>
2020-04-16 17:57:41 +0530

    fix: add cssPath and jsPath

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 
 /* GET static pages. */
-router.get("*", function(req, res) {
-  res.render("index", { title: "The newsletter app" });
+router.get("*", function (req, res) {
+  const cssPath =
+    process.env.NODE_ENV == "production"
+      ? `/dist/bundle/bundle.css`
+      : "/static/bundle.css";
+  const jsPath =
+    process.env.NODE_ENV == "production"
+      ? `/dist/bundle/bundle.js`
+      : "/static/bundle.js";
+  res.render("index", {
+    title: "Ck newsletter | Connected",
+    jsPath,
+    cssPath,
+  });
 });
 
 module.exports = router;
